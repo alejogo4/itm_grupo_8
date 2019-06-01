2599efabdb6db896cfb0a49a9cba168b11e1e2b4
Miguel <mochoa0406@hotmail.com>
2019-05-31 22:02:19 -0500

    Se añadieron los achivos TemplateCorreo.php y EnviarcorreoPrueba Para probar el envio del correo en el servidor

---
+++ TemplateCorreo.php
@@ -0,0 +1,5 @@
+<form action="php/enviarcorreoprueba.php" method="POST">
+    <input name="Nombre" type="text" value ="Juan Carlos Marin"><br> 
+    <input name="Email" type="text" value ="juanscales@gmail.com"><br> 
+    <input type="submit" value="Enviar Correo">
+</form>
